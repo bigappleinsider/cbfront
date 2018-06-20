6e41291da1ce6f4166efff6b624985e8a1abe7e5
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2018-06-20 15:53:49 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Search endpoint by tag
+
+[Screenshot]: https://github.com/bigappleinsider/cbfront/raw/master/docs/screenshot.png "Screenshot"
--- src/containers/Grid/index.js
+++ src/containers/Grid/index.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
 
 import { fetchSearch } from '../../actions/searchActions';
-
 import SearchForm from '../../components/SearchForm';
+
+const styles = {
+  codeWrap: {
+    margin: 10,
+  }
+};
 
 class Grid extends Component {
   handleSearch(endpoint, tag) {
@@ -13,18 +19,29 @@ class Grid extends Component {
     dispatch(fetchSearch(endpoint, tag));
   }
   render() {
-    const { data, isLoading } = this.props;
+    const { data, isLoading, classes } = this.props;
     const keys = Object.keys(data)
     return(
       <div>
         <SearchForm handleSearch={this.handleSearch.bind(this)} />
         {isLoading && <CircularProgress size={50} />}
+        {!isLoading &&
+            !keys.length && (
+              <Typography variant="headline" component="h3">
+                Use form above to search URL for tags
+              </Typography>
+            )}
         <div>
-          {keys.length>0 && data[keys[0]].map(item => {
+          {keys.length>0 && data[keys[0]].map((item, idx) => {
             return (
+             <div className={classes.codeWrap} key={idx}> 
               <Typography variant="title">
               {item.innerText}
               </Typography>
+              <code>
+                {item.innerHtml}
+              </code>
+            </div>
             );
           })}
         </div>
@@ -41,4 +58,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(Grid);
+export default withStyles(styles)(connect(mapStateToProps)(Grid));
